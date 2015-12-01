174ad95d62edf21d9adad47de2cd76a93f284ba2
Wellsen <gengshengzhang@gmail.com>
2015-12-01 10:42:03 +0800

    set revision to 1.0.2

--- Include/revision.h
+++ Include/revision.h
@@ -29,8 +29,8 @@
 // Use no http/rtsp tspecial chars in kVersionString and kBuildString defines
 // tspecials =  ()<>@,;:\/"[]?=
 
-#define kVersionString "1.0.1"
-#define kBuildString "15.1105"
+#define kVersionString "1.0.2"
+#define kBuildString "15.1201"
 
 // Use kCommentString for seed or other release info 
 // Do not use '(' or ')' in the kCommentString
