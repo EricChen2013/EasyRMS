7284351b08ae4db733629263785b418e0d80f89f
Wellsen <gengshengzhang@gmail.com>
2015-12-01 10:45:30 +0800

    EasyRMS Linux 1.0.2

--- EasyRMS/Server.tproj/main.cpp
+++ EasyRMS/Server.tproj/main.cpp
@@ -214,7 +214,19 @@ int main(int argc, char * argv[])
 {
     extern char* optarg;
     
-    
+#if _DEBUG
+    char* compileType = "Compile_Flags/_DEBUG; ";
+#else
+   char* compileType = "Compile_Flags/_RELEASE;";
+#endif
+	qtss_printf("%s/%s ( Build/%s; Platform/%s; %s%s) Built on: %s\n",
+										QTSServerInterface::GetServerName().Ptr,
+                                        QTSServerInterface::GetServerVersion().Ptr,
+                                        QTSServerInterface::GetServerBuild().Ptr,
+                                        QTSServerInterface::GetServerPlatform().Ptr,
+                                        compileType,
+                                        QTSServerInterface::GetServerComment().Ptr,
+                                        QTSServerInterface::GetServerBuildDate().Ptr);
 
     // on write, don't send signal for SIGPIPE, just set errno to EPIPE
     // and return -1
